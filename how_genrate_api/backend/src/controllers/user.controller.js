cf68edca48be4e3a0c31601cb8a356803a03609a
ehdaa12s <112123984+ehdaa12s@users.noreply.github.com>
2026-01-31 14:44:00 +0200

    steps and arc to build api

---
+++ how_genrate_api/backend/src/controllers/user.controller.js
@@ -0,0 +1,21 @@
+import User from "../models/user.model.js";
+
+// GET all users
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// CREATE user
+export const createUser = async (req, res, next) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
